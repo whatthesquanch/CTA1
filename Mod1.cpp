06c3d532933e9fbd63930d029f7232825b8e2d86
whatthesquanch <146876837+whatthesquanch@users.noreply.github.com>
2024-04-17 18:17:45 -1000

    Add files via upload

---
+++ Mod1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+// Standard namespace declaration
+using namespace std;
+
+/// Main Function
+int main()
+{
+
+    // Standard Output Statement
+    std::cout << "Welcome to this C++ Program" << std::endl;
+
+    std::cout << "I have corrected all errors for this program." << std::endl;
+
+    // Wait for Output Screen
+    std::cin.get();
+
+    // Main Function return Statement
+    return 0;
+}
