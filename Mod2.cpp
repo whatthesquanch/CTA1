6d2bf271849e93457de513ad0400d31277c69e53
whatthesquanch <146876837+whatthesquanch@users.noreply.github.com>
2024-04-17 18:47:30 -1000

    Add files via upload

---
+++ Mod2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    double myMoney = 1000.50; 
+
+    // Standard Output Statements
+
+    cout << "Please be sure to correct all syntax errors in this program" << endl;
+    cout << "I have corrected all errors for this program." << endl;
+    cout << "The total amount of money available is = " << myMoney << endl;
+
+    // Wait for output screen
+    cin.get();
+
+    // Main function return statement
+    return 0;
+}
